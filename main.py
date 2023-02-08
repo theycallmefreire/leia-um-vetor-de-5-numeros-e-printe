b3ae4f172a90e7efaf746c561ecc90ec1f0be7d3
Guilherme B. Freire <109501699+theycallmefreire@users.noreply.github.com>
2023-02-08 15:52:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+v = []
+for i in range(5):
+    n = int(input("digite um numero: "))
+    v.append(n)
+print(v)
