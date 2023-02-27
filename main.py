8ba690d4ccdcf744b90e1c1969a3bd0be65f1649
Guilherme B. Freire <109501699+theycallmefreire@users.noreply.github.com>
2023-02-27 15:15:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+#vetor indefinida
 v = []
-for i in range(5):
-    n = int(input("digite um numero: "))
+#laço que conta até 10
+for i in range(10):
+  #pergunta o numero pra o usuario
+    n = float(input(f"{i+1} digite um numero: "))
+  #add esse numero no vetor "v"
     v.append(n)
+#printa o vetor
 print(v)
